633a8650cdafe8b3563da8f78649be1ea7b12920
Lucas Iniesta Simoes <l.iniesta.94@gmail.com>
2025-09-12 22:13:06 -0300

    Commit iniciail

---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from '../config/database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(databaseConfig)],
+      inject: [databaseConfig.KEY],
+      useFactory: (
+        databaseConfigurations: ConfigType<typeof databaseConfig>,
+      ) => {
+        return {
+          type: databaseConfigurations.type,
+          host: databaseConfigurations.host,
+          port: databaseConfigurations.port,
+          username: databaseConfigurations.username,
+          password: databaseConfigurations.password,
+          database: databaseConfigurations.database,
+          autoLoadEntities: databaseConfigurations.autoLoadEntities,
+          synchronize: databaseConfigurations.synchronize,
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
